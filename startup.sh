d0fce75e07f32ee6a743129db283dbba20d93ca3
clintkev251 <43455780+clintkev251@users.noreply.github.com>
2022-05-14 20:16:02 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,9 +6,9 @@ export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
 CONFIG=./config/trash.yml
 if test ! -f "$CONFIG"; then
-    ./trash create-config --path ./config/trash.yml
+    ./recyclarr create-config --path ./config/trash.yml
 else
-    ./trash radarr --config ./config/trash.yml
-    ./trash sonarr --config ./config/trash.yml
+    ./recyclarr radarr --config ./config/trash.yml
+    ./recyclarr sonarr --config ./config/trash.yml
 fi
 # tail -f /dev/null
