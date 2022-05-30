cc74642d9e23932eb1bcb891d25d6435004951dc
Kevin Clinthorne <clintkev251@gmail.com>
2022-05-29 22:33:00 -0400

    Added optional cron scheduling, improved logging

--- readme.md
+++ readme.md
@@ -12,9 +12,12 @@ services:
     environment:
       RUN_RADARR: true  # Optional - If unset, defaults to true
       RUN_SONARR: true  # Optional - If unset, defaults to true
+      CRON_EXPRESSION: "0 15 * * *" # Optional - The cron expression that determines when the script will be run. If unset, scripts will be run at startup of container
 ```
 ### Usage
-On first run, the script will look for a config file in your mapped config directory. If one does not exist it will create one and exit. You can edit this file to point to your radarr and sonarr instances and use the desired profiles. On subsequent starts, dependent on the enviornment vars you have set `recyclarr radarr` and `recyclarr sonarr` will be run and the container will exit. You can write a simple script to start the container at your desired interval to update profiles.
+On first run, the script will look for a config file in your mapped config directory. If one does not exist it will create one and exit. You can edit this file to point to your radarr and sonarr instances and use the desired profiles. 
+
+On subsequent starts, the behavior will be dependent on whether or not you've defined a cron expression in your environment varriables. If a cron expression is defined, the approprate commands will be run on the schedule that you've defined. If not, they will be run once and the container will exit. Using this mode you can write a simple script to start the container at your desired interval from your host.
 
 This is autobuilt and published on new releases from the main project. Pull requests for fixes and enhancements are welcomed.
 
--- startup.sh
+++ startup.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-
+export CONFIG=/etc/trash/config
 cd /etc/trash
 
-export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
-
+# Test if config exists. If not, create it and exit
+if test ! -f "$CONFIG/recyclarr.yml"; then
+    ./exec create-config --path $CONFIG
+    exit 0
+fi
+# Check if user has explicitly set RUN_* vars, if not, set to default
 if test -z "$RUN_RADARR"; then
     RUN_RADARR="true"
 fi
 if test -z "$RUN_SONARR"; then
     RUN_SONARR="true"
 fi
-
-CONFIG=./config/recyclarr.yml
-
-if test ! -f "$CONFIG"; then
-    ./recyclarr-exec create-config --path $CONFIG
-else
+# Check if user has set a cron expression. If so set crontab and run cron in forground mode. If not, run onetime commands
+if test -n "$CRON_EXPRESSION"; then
+    echo "Building cron expressions using $CRON_EXPRESSION"
     if $RUN_RADARR = "true"; then
-        ./recyclarr-exec radarr --config $CONFIG
+        echo "Setting up job for Radarr"
+        echo "$CRON_EXPRESSION /etc/trash/exec radarr --config $CONFIG/recyclarr.yml >/proc/1/fd/1 2>/proc/1/fd/2" | crontab
     fi
     if $RUN_SONARR = "true"; then
-    ./recyclarr-exec sonarr --config $CONFIG
+        echo "Setting up job for Sonarr"
+        echo "$CRON_EXPRESSION /etc/trash/exec sonarr --config $CONFIG/recyclarr.yml >/proc/1/fd/1 2>/proc/1/fd/2" | crontab
+    fi
+    echo "Starting cron"
+    exec cron -f
+else
+    echo "No cron expression provided, running in external schedule mode"
+    if $RUN_RADARR = "true"; then
+        ./exec radarr --config $CONFIG/recyclarr.yml
+    fi
+    if $RUN_SONARR = "true"; then
+        ./exec sonarr --config $CONFIG/recyclarr.yml
     fi
 fi
-
-# tail -f /dev/null
